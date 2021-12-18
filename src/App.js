253530c762cf87e1d5a362b11ea27fb5c97a9f96
Phon1209 <ptphon@gmail.com>
2021-12-18 12:38:40 -0400

    Change workspace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.scss";
+import "./output.css";
 import Home from "./components/home";
 
 const App = () => {
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./../App.scss";
+import "./../output.css";
 
 const Home = () => {
   return (
