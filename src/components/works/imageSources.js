c613ac36f2f17da8de78dfa18afe7b916b0acda4
Nantita Chomsiri <nantitakikk@gmail.com>
2021-12-22 01:46:38 +0700

    add works

--- src/components/works/imageSources.js
+++ src/components/works/imageSources.js
@@ -1,4 +1,10 @@
 import Exigent from "./../../assets/images/exigent.png";
 import Fortune from "./../../assets/images/fortune1.png";
+import Fortune1 from "./../../assets/images/fortune168_3.png";
+import Fortune3 from "./../../assets/images/fortune168_5.png";
+import Fortune4 from "./../../assets/images/fortune168_6.png";
+import Fortune5 from "./../../assets/images/fortune168_7.png";
+import Fortune6 from "./../../assets/images/fortune168_8.png";
 
-export const images = [{ img: Exigent }, { img: Fortune }];
+export const images = [{ img: Fortune1 }, { img: Fortune3 }, { img: Fortune4 }, { img: Fortune5 }
+    , { img: Fortune6 }];
