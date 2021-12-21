e99b6616ad7d01bb7661ed2249f05a8da4a7a010
Nantita Chomsiri <nantitakikk@gmail.com>
2021-12-22 02:11:35 +0700

    add exigent images

--- src/components/works/imageSources.js
+++ src/components/works/imageSources.js
@@ -5,6 +5,13 @@ import Fortune3 from "./../../assets/images/fortune168_5.png";
 import Fortune4 from "./../../assets/images/fortune168_6.png";
 import Fortune5 from "./../../assets/images/fortune168_7.png";
 import Fortune6 from "./../../assets/images/fortune168_8.png";
+import Exigent1 from "./../../assets/images/exigent1.png";
+import Exigent2 from "./../../assets/images/exigent2.png";
+import Exigent3 from "./../../assets/images/exigent3.png";
+import Exigent4 from "./../../assets/images/exigent4.png";
+import Exigent5 from "./../../assets/images/exigent5.png";
+
 
 export const images = [{ img: Fortune1 }, { img: Fortune3 }, { img: Fortune4 }, { img: Fortune5 }
-    , { img: Fortune6 }];
+    , { img: Fortune6 }, { img: Exigent1 }, { img: Exigent2 }, { img: Exigent3 }, { img: Exigent4 }
+    , { img: Exigent5 }];
