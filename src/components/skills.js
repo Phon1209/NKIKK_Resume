63bf07f265b7dc56d031ee8d85f14d76faeb55a6
Nantita Chomsiri <nantitakikk@gmail.com>
2021-12-22 20:23:24 +0700

    edit skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,17 +3,17 @@ import { ReactComponent as Skillsvg } from "./svgs/skill.svg";
 
 const Skills = () => {
     return (
-        <section>
-          <Skillsvg/>
-            <div className="">
-        <span>Skills</span>
-        <div className="">
-          <li>Designing websites</li>
-          <li>Creating responsive website wireframes</li>
-          <li>Designing app wireframes</li>
-          <li>Writing code with HTML, CSS, Python, R, C, C++</li>
+        <section className='flex flex-col md:flex-row'>
+        <Skillsvg/>
+         <div className="p-5">
+              <span className="text-3xl font-bold">Skills</span>
+              <div className="p-5">
+                <li>Designing websites</li>
+                <li>Creating responsive website wireframes</li>
+                <li>Designing app wireframes</li>
+                <li>Writing code with HTML, CSS, Python, R, C, C++</li>
+              </div>
         </div>
-      </div>
         </section>
     );
 };
