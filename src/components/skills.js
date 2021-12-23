8da3e439a37a64b61489ba4539d262538a273337
Nantita Chomsiri <nantitakikk@gmail.com>
2021-12-23 18:54:09 +0700

    arrange skill section

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,10 +3,10 @@ import { ReactComponent as Skillsvg } from "./svgs/skill.svg";
 
 const Skills = () => {
     return (
-        <section className='flex flex-col md:flex-row'>
+        <section className='flex flex-col md:flex-row items-center justify-between w-full max-w-5xl mx-auto'>
         <Skillsvg/>
-         <div className="py-4">
-              <span className="text-3xl font-bold">Skills</span>
+         <div className="p-10">
+              <span className="text-3xl font-bold text-center block mx-auto my-6">Skills</span>
               <div className="p-5">
                 <li>Designing websites</li>
                 <li>Creating responsive website wireframes</li>
