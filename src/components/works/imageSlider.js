52197cda8eed41b352d73cbd11cf89afd6f404af
Nantita Chomsiri <nantitakikk@gmail.com>
2021-12-28 11:37:58 +0700

    page dot and animation
    Co-authored-by: Phon1209 <Phon1209@users.noreply.github.com>

--- src/components/works/imageSlider.js
+++ src/components/works/imageSlider.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import PropTypes from "prop-types";
 import { imageSet } from "./imageSources";
 
+const Dot = ({ current }) => {
+  return (
+    <div
+      className={`${current ? "bg-pink-400 w-2 h-2" : "bg-gray-200 w-1.5 h-1.5"} 
+                  rounded-full  m-2`}
+    />
+  );
+};
+
 const ImageSlider = ({ imageID }) => {
   const images = imageSet[imageID];
+  const Slider = useRef(null);
   const [currImg, setCurrImg] = useState(0);
 
-  const changeImage = (event, indexChange) => {
+  const changeImage = (indexChange) => {
     let targetIndex = (currImg + indexChange) % images.length;
     if (targetIndex < 0) targetIndex += images.length;
-    const panel = event.target.parentNode.parentNode.parentNode;
+    const panel = Slider.current;
     setTimeout(() => {
       panel.classList.add("opacity-0");
       setTimeout(() => setCurrImg(targetIndex), 150);
@@ -19,44 +29,51 @@ const ImageSlider = ({ imageID }) => {
     }, 0);
   };
 
-  return (
-    <div className="image-slider transition-opacity">
-      <div className="hoverLeft">
-        <div
-          className="image-prev opacity-0 hover:opacity-100"
-          onClick={(e) => changeImage(e, -1)}
-        >
-          <div className="w-full top-0 left-0 h-full"></div>
-          <ion-icon name="arrow-back-outline"></ion-icon>
-        </div>
+  const renderDots = () => {
+    return [...Array(images.length).keys()].map((index) => {
+      return <Dot key={index} current={currImg === index} />;
+    });
+  };
 
-      </div>
-      <div className="hoverRight">
-        <div
-          className="image-next opacity-0 hover:opacity-100"
-          onClick={(e) => changeImage(e, 1)}
-        >
-          <div className="w-full top-0 left-0 h-full"></div>
-          <ion-icon name="arrow-forward-outline"></ion-icon>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="image-slider transition-opacity" ref={Slider}>
+        <div className="hoverLeft">
+          <div
+            className="image-prev opacity-0 hover:opacity-100"
+            onClick={(e) => changeImage(-1)}
+          >
+            <div className="w-full top-0 left-0 h-full"></div>
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
         </div>
+        <div className="hoverRight">
+          <div
+            className="image-next opacity-0 hover:opacity-100"
+            onClick={(e) => changeImage(1)}
+          >
+            <div className="w-full top-0 left-0 h-full"></div>
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+        </div>
+        <img
+          src={`${images[currImg].img}`}
+          alt={`Set ID: ${imageID}`}
+          width="800"
+          height="600"
+          style={{
+            height: "auto",
+            width: "100%",
+          }}
+        />
       </div>
-      <img
-      src={`${images[currImg].img}`}
-      alt={`Set ID: ${imageID}`}
-      width="800"
-      height="600"
-        style={{
-          height:"auto",
-          width: "100%",
-        }}
-    />
+      <div className="flex m-4 justify-center items-center">{renderDots()}</div>
     </div>
-     
   );
 };
 
 ImageSlider.propTypes = {
   imageID: PropTypes.number,
-}
+};
 
 export default ImageSlider;
